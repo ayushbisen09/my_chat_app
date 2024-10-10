2dc1b584d4b2b79c6c1611f50ec634ce52e4e9c5
ayushbisen09 <ayushbisen1029@gmail.com>
2024-10-10 11:05:31 +0530

    Added Latest

--- src/sections/quick-replies/component/table/quick-replies-table-row.jsx
+++ src/sections/quick-replies/component/table/quick-replies-table-row.jsx
@@ -119,7 +119,7 @@ export function QuickRepliesTableRow({ row, selected, onSelectRow, quickrepliesI
               <Box
                 component="span"
                 sx={{
-                  maxWidth: 150,
+                  maxWidth: 500,
                   overflow: 'hidden',
                   textOverflow: 'ellipsis',
                   whiteSpace: 'nowrap',
